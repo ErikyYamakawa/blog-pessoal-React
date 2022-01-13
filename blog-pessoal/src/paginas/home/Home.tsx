f280abdb8da6d59a04fa612b0a01ec88931f85e3
Eriky Yamakawa <erikyyamakawa123@gmail.com>
2022-01-13 12:12:47 -0300

    blog pessoal React

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">home</h1>
+            <img src="" alt="Imagem Tela inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
